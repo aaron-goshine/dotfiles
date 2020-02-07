8c5e959d278f2527a72fa7ca6af73dd1d163155a
Aaron Goshine <greenlig@gmail.com>
2020-02-07 20:11:22 +0000

    fix: clean up

--- bin/pass-i.sh
+++
@@ -1,5 +0,0 @@
-echo '6F2O*6wnXGJL
-User name: goshine-cli
-Access key ID: AKIAZB6LZ3IUC6GZI767
-Secret access key: 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
-Console login: 622660803112.signin.aws.amazon.com/console' | pass insert -m GD/aws/cli
