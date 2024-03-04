f6ddb8b6937f9bef62ee60c79132706ace46d373
Aaron Goshine <greenlig@gmail.com>
2024-03-04 12:25:50 +0000

    feat: add new shell scripting function

---
+++ server.sh
@@ -0,0 +1,3 @@
+local port="${1:-8000}" 
+open "http://localhost:${port}/"
+python3 -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
